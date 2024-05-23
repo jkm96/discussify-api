31232f89e1e273a010187995bab26274db905808
jkmdev <jkm96.dev@gmail.com>
2024-05-23 22:52:25 +0300

    feat: add command to clear expired tokens

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,10 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-//Artisan::command('inspire', function () {
-//    $this->comment(Inspiring::quote());
-//})->purpose('Display an inspiring quote')->hourly();
-
 Schedule::command('app:forum-statistics-command')
     ->everyFiveMinutes()
+    ->runInBackground();
+
+Schedule::command('app:clear-expired-tokens')
+    ->dailyAt('01:00')
     ->runInBackground();
